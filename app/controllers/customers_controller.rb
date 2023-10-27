9a9af00eb3bef4907ed88e5e6865b5979c4c8bff
Israelgrs <israel_grs@live.com>
2023-10-26 23:46:39 -0300

    Using strong parameters to test authenticated post requests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -59,13 +59,14 @@ class CustomersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      @customer = Customer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.require(:customer).permit(:name, :email)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def customer_params
+    params.require(:customer).permit(:name, :email, :address)
+  end
 end
